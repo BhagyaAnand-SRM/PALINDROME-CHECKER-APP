e26e665eb3232d44c7d457e2eec7beca56681f3e
BhagyaAnand-SRM <ba9259@srmist.edu.in>
2026-02-27 10:43:01 +0530

    UC2:Hardcoded Palindrome

---
+++ HardcodedPalindrome.java
@@ -0,0 +1,15 @@
+public class HardcodedPalindrome {
+  public static void main(String[] args) {
+    String input = "madam";
+    boolean result = true;
+    for (int i = 0; i < input.length() / 2; i++) {
+      if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+        result = false;
+        break;
+      }
+    }
+
+    System.out.println("Is it a Palindrome?: " + result);
+  }
+
+}
